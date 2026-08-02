98947e41f1509344186d79da79ef982fb596110c
emomo81 <emmanuelmomo81@gmail.com>
2026-08-02 23:49:11 +0200

    new files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tanstackStart(), viteReact(), tailwindcss(), tsconfigPaths()],
+  plugins: [tailwindcss(), tanstackStart(), nitro(), viteReact(), tsconfigPaths()],
 });
